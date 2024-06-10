a2ce64bfecaefbe65f492bb38790c0a2bde868dd
Xuejiao-Luo <luoluoxj@gmail.com>
2024-06-10 16:26:15 +0200

    add test examples

--- utils/data_loading_brdf.py
+++ utils/data_loading_brdf.py
@@ -35,7 +35,7 @@ class BasicDataset(Dataset):
     def __getitem__(self, idx):
         name = self.ids[idx]
 
-        fileExt = '.jpg'
+        fileExt = '.png'
         img_brdfs_file = Path(self.images_dir, name+fileExt)
 
         if self.train:
